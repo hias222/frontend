47510242b26077336ac879e6a756ab1fb85d9763
matthias <matthias.fuchs>
2021-08-29 19:33:21 +0200

    url

--- src/pages/lists.tsx
+++ src/pages/lists.tsx
@@ -7,7 +7,7 @@ export default class lists extends React.Component<{}, {}> {
     render() {
         return (
             <Container maxWidth="lg">
-                <Iframe url="https://wssplash.azurewebsites.net"
+                <Iframe url="https://d28o763yj8kyv0.cloudfront.net"
                     height="1000"
                     width="100%"
                     id="myId"
